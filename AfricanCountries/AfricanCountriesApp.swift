c786e5ad40c9ef75130cbf69b851848026760c34
Jerry Nkongolo <jerrynkongolo@icloud.com>
2023-05-22 18:11:15 +0300

    Initial Commit

---
+++ AfricanCountries/AfricanCountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricanCountriesApp.swift
+//  AfricanCountries
+//
+//  Created by jerry nkongolo on 2023-05-22.
+//
+
+import SwiftUI
+
+@main
+struct AfricanCountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
